cb296a74182de614271f2e56342ec1f9d9a37ee8
Haider Ali <haiderkhalid602@gmail.com>
2023-05-23 01:21:04 +0500

    Necessary configuration files

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    function index(){
+
+    }
+
+    function showMax(){
+
+    }
+
+    function showMin(){
+
+    }
+
+    function showAvg(){
+        
+    }
+}
